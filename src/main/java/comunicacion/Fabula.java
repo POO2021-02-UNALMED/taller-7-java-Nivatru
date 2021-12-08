8049e67efb1122a2038213d6d8002f3f79f3e8d5
Nicolás Valencia Trujillo <nvalenciat@unal.edu.co>
2021-12-07 21:47:10 -0500

    a

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -2,25 +2,25 @@ package comunicacion;
 
 public class Fabula extends Escrito{
 	
-	private String enseñanza;
+	private String ensenanza;
 	private String interpretacion;
 	
-	public Fabula(String origen, String titulo, String autor, int paginas, String enseñanza, String interpretacion) {
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
 		super(origen, titulo, autor, paginas);
-		this.enseñanza = enseñanza;
+		this.ensenanza = ensenanza;
 		this.interpretacion = interpretacion;
 	}
 
-	public String getEnseñanza() {
-		return enseñanza;
+	public String getEnsenanza() {
+		return ensenanza;
 	}
 
 	public String getInterpretacion() {
 		return interpretacion;
 	}
 
-	public void setEnseñanza(String enseñanza) {
-		this.enseñanza = enseñanza;
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
 	}
 
 	public void setInterpretacion(String interpretacion) {
@@ -32,7 +32,7 @@ public class Fabula extends Escrito{
 	}
 	
 	public String toString() {
-		String str = String.format("%origen\n%titulo\n%autor\n%paginas\n%enseñanza", getOrigen(), getTitulo(), getAutor(), getPaginas(), this.enseñanza);
+		String str = String.format("%origen\n%titulo\n%autor\n%paginas\n%ensenanza", getOrigen(), getTitulo(), getAutor(), getPaginas(), this.ensenanza);
 		return str;
 	}
 	
